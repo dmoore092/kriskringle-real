b83ff580c9f5d22918611a60214df541c50d0c6a
Dale Moore <dmoore092@gmail.com>
2019-05-30 16:55:29 -0400

    working on db

--- main.php
+++ main.php
@@ -42,9 +42,9 @@
 <?php
     //get preferences
     $mysqli = new mysqli("127.0.0.1", "root", "", "kriskringle");
-    if ($mysqli->connect_errno) {
-        echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-    }
+    // if ($mysqli->connect_errno) {
+    //     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    // }
     // $name = $_POST['name-see'];
     // if(isset($_POST['see-prefs'])){
     //     $results = $mysqli->query("SELECT * FROM preferences WHERE name = '{$name}';");
