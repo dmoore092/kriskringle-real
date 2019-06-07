b985f991bb22059b6763d1ef58c5b9fc92b8a07e
Dale Moore <dmoore092@gmail.com>
2019-06-07 12:52:56 -0400

    added analytics

--- assets/inc/head.inc.php
+++ assets/inc/head.inc.php
@@ -6,16 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="author" content="Dale Moore">
     <meta name="decription" content="Yearly Kris Kringle Gift Exchange"> 
-    <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141642555-1"></script>
     <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-    gtag('config', 'UA-141642555-1');
+        gtag('config', 'UA-141642555-1');
     </script>
-
     <link rel="stylesheet" href="/assets/css/index.css">
     <link rel="stylesheet" href="/assets/css/main.css">
     <title>Kris Kringle 2019!</title>
