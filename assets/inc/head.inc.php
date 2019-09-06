d63ab5fa899927b1829ef8fd97a3c6a1ae63b2e6
Dale Moore <dmoore092@gmail.com>
2019-09-06 11:44:24 -0400

    moved tracking info to correct place

--- assets/inc/head.inc.php
+++ assets/inc/head.inc.php
@@ -1,11 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="author" content="Dale Moore">
-    <meta name="decription" content="Yearly Kris Kringle Gift Exchange"> 
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141642555-1"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
@@ -14,6 +9,11 @@
 
         gtag('config', 'UA-141642555-1');
     </script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Dale Moore">
+    <meta name="decription" content="Yearly Kris Kringle Gift Exchange"> 
     <link rel="stylesheet" href="/assets/css/index.css">
     <link rel="stylesheet" href="/assets/css/main.css">
     <title>Kris Kringle 2019!</title>
