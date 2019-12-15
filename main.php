3518a1b239dcc2616d20bb5f634fb36b2719f86a
Dale Moore <dmoore092@gmail.com>
2019-12-15 10:18:27 -0500

    fix typo

--- main.php
+++ main.php
@@ -60,21 +60,15 @@
             <div class='show-prefs'>
             <p> <?php echo $row['name'] ?>'s first preference:<br> <?php echo $row['pref1']?>
             <br>
-        <?php if($row['pref1link'] != ""){ ?>
-            <a href='<?php echo $row['pref1link']?>' target='_blank'>Link</a>
-        <?php } ?>
+        <?php if($row['pref1link'] != ""){ ?><a href='<?php echo $row['pref1link']?>' target='_blank'>Link</a><?php } ?>
             </p>
             <p> <?php echo $row['name']?>'s second preference:<br> <?php echo $row['pref2']?>
             <br>
-            <?php if($row['pref2ink'] != ""){ ?>
-            <a href='<?php echo $row['pref2link']?>' target='_blank'>Link</a>
-        <?php } ?>
+        <?php if($row['pref2link'] != ""){ ?><a href='<?php echo $row['pref2link']?>' target='_blank'>Link</a><?php } ?>
             </p>
             <p><?php echo $row['name']?>'s third preference:<br> <?php echo $row['pref3']?>
             <br>
-            <?php if($row['pref3link'] != ""){ ?>
-            <a href='<?php echo $row['pref3link']?>' target='_blank'>Link</a>
-        <?php } ?>
+        <?php if($row['pref3link'] != ""){ ?><a href='<?php echo $row['pref3link']?>' target='_blank'>Link</a><?php } ?>
             </p>
         </div>  
 <?php        }
