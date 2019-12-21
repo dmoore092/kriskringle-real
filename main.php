d2c46f09639653b683de5a73183154055f7d7372
Dale Moore <dmoore092@gmail.com>
2019-12-21 10:03:20 -0500

    adding the ability to add notes to your preferences

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <?php include 'assets/inc/head.inc.php'; 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(0);
 ?>
 
     <div>
@@ -64,17 +64,17 @@ error_reporting(E_ALL);
             <div class='show-prefs'>
             <p> <?php echo $row['name'] ?>'s first preference:<br> <?php echo $row['pref1']?>
             <br>
-        <?php if($row['pref1note'] != ""){ ?> Note: <?php echo $row['pref1note'] ?> <?php }?>
+        <?php if($row['pref1note'] != ""){ ?> Note: <?php echo $row['pref1note'] ?><br> <?php }?>
             <?php if($row['pref1link'] != ""){ ?><a href='<?php echo $row['pref1link']?>' target='_blank'>Link</a><?php } ?>
             </p>
             <p> <?php echo $row['name']?>'s second preference:<br> <?php echo $row['pref2']?>
             <br>
-        <?php if($row['pref2note'] != ""){ ?> Note: <?php echo $row['pref2note'] ?> <?php }?>
+        <?php if($row['pref2note'] != ""){ ?> Note: <?php echo $row['pref2note'] ?><br> <?php }?>
             <?php if($row['pref2link'] != ""){ ?><a href='<?php echo $row['pref2link']?>' target='_blank'>Link</a><?php } ?>
             </p>
             <p><?php echo $row['name']?>'s third preference:<br> <?php echo $row['pref3']?>
             <br>
-        <?php if($row['pref3note'] != ""){ ?> Note: <?php echo $row['pref3note'] ?> <?php }?>
+        <?php if($row['pref3note'] != ""){ ?> Note: <?php echo $row['pref3note'] ?><br> <?php }?>
             <?php if($row['pref3link'] != ""){ ?><a href='<?php echo $row['pref3link']?>' target='_blank'>Link</a><?php } ?>
             </p>
         </div>  
